a0b57922ac7f6d2089f29ba0912878ad4a611a8a
devinit21 <sackdevon@gmail.com>
2026-03-25 12:37:29 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,1 +1,10 @@
+import os
 
+RENDER_API_KEY = os.getenv("RENDER_API_KEY", "")
+RENDER_API_BASE = os.getenv("RENDER_API_BASE", "").rstrip("/")
+
+MAX_ACTIVE_JOBS = int(os.getenv("MAX_ACTIVE_JOBS", "10"))
+LAUNCH_TIMEOUT_SEC = int(os.getenv("LAUNCH_TIMEOUT_SEC", "20"))
+
+MIN_PROFIT_USD = float(os.getenv("MIN_PROFIT_USD", "0.50"))
+PROFIT_MULTIPLIER = float(os.getenv("PROFIT_MULTIPLIER", "2.0"))

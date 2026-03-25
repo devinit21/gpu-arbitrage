b4bc4f1d674b9532d7df62434222f2425844c77e
devinit21 <sackdevon@gmail.com>
2026-03-25 12:34:49 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
-
+class GpuOffer:
+    def __init__(
+        self,
+        provider,
+        offer_id,
+        gpu_type,
+        hourly_price,
+        reliability,
+        is_spot,
+        region,
+    ):
+        self.provider = provider
+        self.offer_id = offer_id
+        self.gpu_type = gpu_type
+        self.hourly_price = hourly_price
+        self.reliability = reliability
+        self.is_spot = is_spot
+        self.region = region
+        self.effective_hourly_cost = hourly_price
